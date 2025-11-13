bbe079f3c9e5b0c10cd6b64aade9ffaa8cc4f9ea
Anniina-55 <anniina.leikas55@gmail.com>
2025-11-13 18:12:17 +0200

    Added seat-map feature and installed swagger

---
+++ swagger.js
@@ -0,0 +1,27 @@
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Movie Theatre Staff API',
+      version: '1.0.0',
+      description: 'API documentation for staff backend',
+    },
+    servers: [
+      { url: 'http://localhost:3000', description: 'Local server' }
+    ],
+  },
+  // finds all route-files with swagger-comments
+  apis: ['./src/routes/*.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+function setupSwagger(app) {
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  console.log('Swagger docs available at http://localhost:3000/api-docs');
+}
+
+module.exports = setupSwagger;
